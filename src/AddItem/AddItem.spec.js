66ad5c2b4dea5956d9d34b4b3c6f861c79a41b39
Egozi <egozieran@gmail.com>
2020-03-18 11:21:00 +0200

    (WSR 8) Move types tests to folders (#5205)

--- src/AddItem/AddItem.spec.js
+++ src/AddItem/AddItem.spec.js
@@ -5,8 +5,8 @@ import {
   cleanup,
 } from '../../test/utils/react';
 
-import { addItemPrivateDriverFactory } from './tests/AddItem.private.driver';
-import { addItemPrivateUniDriverFactory } from './tests/AddItem.private.uni.driver';
+import { addItemPrivateDriverFactory } from './test/AddItem.private.driver';
+import { addItemPrivateUniDriverFactory } from './test/AddItem.private.uni.driver';
 
 import AddItem from './AddItem';
 
---
+++ src/ToggleButton/test/ToggleButton.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import ToggleButton from '..';
+import { toggleButtonTestkitFactory } from '../../../testkit';
+import { toggleButtonTestkitFactory as toggleButtonEnzymeTestkitFactory } from '../../../testkit/enzyme';
+import { toggleButtonTestkitFactory as toggleButtonPuppeteerTestkitFactory } from '../../../testkit/puppeteer';
+import * as enzyme from 'enzyme';
+import * as puppeteer from 'puppeteer';
+
+function ToggleButtonWithMandatoryProps() {
+  return <ToggleButton />;
+}
+
+function ToggleButtonWithAllProps() {
+  return (
+    <ToggleButton
+      as="a"
+      dataHook="hook"
+      disabled
+      onClick={_ev => {}}
+      selected
+      skin="dark"
+      tooltipContent="content"
+      tooltipProps={{}}
+    />
+  );
+}
+
+async function testkits() {
+  const testkit = toggleButtonTestkitFactory({
+    dataHook: 'hook',
+    wrapper: document.createElement('div')
+  });
+
+  const enzymeTestkit = toggleButtonEnzymeTestkitFactory({
+    dataHook: 'hook',
+    wrapper: enzyme.mount(<div />)
+  });
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const puppeteerTestkit = await toggleButtonPuppeteerTestkitFactory({
+    dataHook: 'hook',
+    page
+  });
+}
