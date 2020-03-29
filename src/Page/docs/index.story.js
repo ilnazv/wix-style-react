fe071b1c69f0b1c1b8d27e247fd39a6ce9ca6356
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-03-29 15:48:08 +0300

    adding keys

--- src/Page/docs/index.story.js
+++ src/Page/docs/index.story.js
@@ -208,8 +208,8 @@ export default {
                           <Card>
                             <Card.Content>
                               <h3>Scroll Down</h3>
-                              {Array(20).fill(' ').map(item => 
-                                (<div>
+                              {Array(20).fill(' ').map((item, i) =>
+                                (<div key={"minimized-header-example-item-"+ i}>
                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
                                   facilisis molestie magna vitae pellentesque. Ut elementum
                                   accumsan nibh, ut faucibus velit. Vestibulum at mollis justo.
@@ -246,8 +246,8 @@ export default {
                         <Col span={8}>
                           <Card>
                             <Card.Content>
-                              {Array(20).fill(' ').map(item => 
-                                (<div>
+                              {Array(20).fill(' ').map((item,i) =>
+                                (<div key={"header-tail-elements-example-item-"+ i}>
                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
                                   facilisis molestie magna vitae pellentesque. Ut elementum
                                   accumsan nibh, ut faucibus velit. Vestibulum at mollis justo.
@@ -277,8 +277,8 @@ export default {
                         <Col span={8}>
                           <Card>
                             <Card.Content>
-                              {Array(20).fill(' ').map(item => 
-                                (<div>
+                              {Array(20).fill(' ').map((item,i) =>
+                                (<div key={"sticky-elements-example-item-"+i}>
                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
                                   facilisis molestie magna vitae pellentesque. Ut elementum
                                   accumsan nibh, ut faucibus velit. Vestibulum at mollis justo.
